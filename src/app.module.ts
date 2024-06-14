fb5afc1bcfc2f19247a15eb5fc973b455e5c7082
Giovani <rang990603@gmail.com>
2024-06-14 12:05:36 -0600

    implementando localidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EstadosModule } from './estados/estados.module';
 import { MunicipiosModule } from './municipios/municipios.module';
+import { LocalidadesModule } from './localidades/localidades.module';
 
 @Module({
-  imports: [EstadosModule, MunicipiosModule],
+  imports: [EstadosModule, MunicipiosModule, LocalidadesModule],
   controllers: [AppController],
   providers: [AppService],
 })
